0b28e52de7a27dec67c6ec4dccf7cc8e08d2d03e
Norberto Jiménez <norberto.j90@gmail.com>
2022-03-08 21:44:40 -0600

    Clase 1

---
+++ Clase 1/js/main.js
@@ -0,0 +1,63 @@
+var nombre = 'Norberto';
+
+var whats = nombre;
+var face = nombre;
+var ig = nombre;
+var tw = nombre;
+
+/* console.log(nombre)
+console.log(whats);
+console.log(face);
+console.log(ig);
+console.log(tw); */
+
+//console.log(prompt('¿Hay alguien ahí?'));
+
+/* var olor = confirm('¿Hueles feo?')
+
+if (olor) {
+    console.log('Fuchi');
+} else {
+    console.log('Gracias por bañarte');
+} */
+
+//Operadores
+var num1 = 98;
+var num2 = 88;
+var num3 = -123;
+
+var suma = num1 + num2;
+var resta = num2 - num1;
+var division = num1 / num2;
+var multi = num1 * num2;
+var modulo = num1 % num2;
+
+
+//Operadores de comparación
+
+var dinero = 300;
+var pcGamer = 800;
+
+/* > mayor que
+< menor que 
+== igual que */
+
+/* if (dinero > pcGamer) {
+    console.log('Te puedes comprar la PC');
+}
+
+if (dinero < pcGamer) {
+    console.log('No te alcanza la PC');
+}
+
+if (dinero == pcGamer) {
+    console.log('Te puedes comprar la PC x2');
+} */
+
+var edad = prompt('¿Qué edad tienes?');
+
+if (edad < 18) {
+    console.log('Uy no puede pasar joven');
+} if (edad >= 18) {
+    console.log('Pásele mi joven');
+}
