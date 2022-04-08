05bea68ff8e15c775653f9b1983c8046ea2e09a4
Norberto Jiménez <norberto.j90@gmail.com>
2022-04-07 19:43:34 -0500

    Tarea 2

---
+++ Tarea 2/js/main.js
@@ -0,0 +1,7 @@
+function mostrar() {
+  document.getElementsByTagName('div')[0].style.display = 'block';
+}
+
+function ocultar() {
+  document.getElementsByTagName('div')[0].style.display = 'none';
+}
