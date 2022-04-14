2f10a50f179ffc4e3356301e216b6487aa6fbca3
Norberto Jiménez <norberto.j90@gmail.com>
2022-04-14 15:59:38 -0500

    Comentarios funciones Tarea 3

--- Tarea 3/js/main.js
+++ Tarea 3/js/main.js
@@ -1,7 +1,7 @@
-/* funciones */
 var usuarios = [];
 var noUsuarios = 0;
 
+//Inicia la recolección de datos de usuario para contrastarlos con el array de usuarios existente
 function init() {
   var user = document.getElementById('user');
   var pass = document.getElementById('pass');
@@ -28,10 +28,12 @@ function init() {
   }
 }
 
+//Recupera el valor colocado en los inputs para enviarlo a un array
 function getInfo(input) {
   return input.value;
 }
 
+//Verifica que no exista un nombre de usuario identico en el array "usuarios"
 function checkInfo(user, pass) {
   var contador = 0;
 
@@ -52,6 +54,7 @@ function checkInfo(user, pass) {
   }
 }
 
+//Registra los datos de usuario en el array "usuarios"
 function saveInfo(info) {
   document.getElementsByClassName('registrado')[0].style.display = 'block';
   document.getElementsByClassName('requerido')[0].style.display = 'none';
@@ -59,24 +62,43 @@ function saveInfo(info) {
   noUsuarios++;
 }
 
+//Borra la información colocada en los inputs para que se pueda volver a ingresar nuevos datos
 function clearInput(user, pass) {
   user.value = '';
   pass.value = '';
 }
 
+//Muestra el modal
 function mostrar() {
   document.getElementsByClassName('modal')[0].style.display = 'flex';
   document.getElementsByClassName('form')[0].style.display = 'none';
 }
 
+//Oculta el modal
 function ocultar() {
   document.getElementsByClassName('modal')[0].style.display = 'none';
   document.getElementsByClassName('form')[0].style.display = 'flex';
 }
 
+//Inicializa las funciones borraContenido y crearTarjetas
 function pintar() {
-  document.getElementsByClassName('cards')[0].replaceChildren('');
+  borrarContenido();
+  crearTarjetas();
+}
 
+//Elimina el contenido del contenedor donde se encuentran las tarjetas de usuario
+//para no duplicarlas cuando intentemos pintarlas
+function borrarContenido() {
+  //No hemos visto este metodo jeje
+  /*  document.getElementsByClassName('cards')[0].replaceChildren(''); */
+
+  var container = document.getElementsByClassName('cards')[0];
+
+  container.innerHTML = '';
+}
+
+//Genera una tarjeta con los datos de usuario y contraseña por cada elemento del array "usuarios"
+function crearTarjetas() {
   for (let index = 0; index < usuarios.length; index++) {
     var card = document.createElement('div');
     card.classList = 'userCard';
