9bc028d1b81f1e5601d1f85e0926171f6415d59a
Norberto Jiménez <norberto.j90@gmail.com>
2022-06-09 21:36:29 -0500

    Clase 15

---
+++ Clase_15/js/main.js
@@ -0,0 +1,55 @@
+let fruits = ['manzana1231', 'pera', 'platano', 'sandia', 'coco'];
+console.log(fruits);
+
+/* delete fruits[0]; */
+
+/* fruits.splice(3); */
+fruits.splice(1,3);
+/* fruits.splice(1,3,'perro'); */
+console.log(fruits);
+
+/* console.log(fruits.pop()); */
+/* let fruits2 = fruits.pop(); */
+
+/* let fruits2 = fruits.shift();
+console.log(fruits); */
+/* let fruits2 = fruits.unshift('perro'); */
+
+/* console.log(fruits2);
+console.log(fruits); */
+
+/* fruits = fruits.slice(2);
+console.log(fruits); */
+
+/* console.log(fruits.toString()); */
+/* console.log(fruits.slice(1,3)); */
+
+/* let string1 = fruits.join(' ');
+console.log(string1);
+console.log(string1.indexOf('a'));
+console.log(string1.lastIndexOf('a'));
+console.log(string1.includes('pera')); */
+/* console.log(string1.length);
+
+console.log(string1.slice(-5,-1));
+
+console.log(string1.slice(2,5));
+console.log(string1.substring(-5,-1));
+console.log(string1.substr(-5,5));
+
+console.log(string1.replace('3','**'));
+console.log(string1.toUpperCase());
+console.log(string1.toLowerCase());
+console.log(string1.toLocaleUpperCase()); */
+
+/* 
+let txt1 = 'perro';
+let txt2 = 'askdhjagsd';
+let txt4 = '         añlsdkjasdlka         ';
+let txt3 = txt1.concat(' ',txt2);
+console.log(txt4);
+console.log(txt4.trim());
+
+console.log(txt1.charAt(4));
+console.log(txt1.charCodeAt(4)); */
+
